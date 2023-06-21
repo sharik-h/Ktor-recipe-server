5d095f88d8790f0855395647fdc1ee5c4a36571c
Mohammed sharikh <mohammedsharik0123@gmail.com>
2023-06-21 07:37:10 +0530

    Fix DELETE function

--- src/main/kotlin/com/example/routes/deleteData.kt
+++ src/main/kotlin/com/example/routes/deleteData.kt
@@ -7,10 +7,11 @@ import io.ktor.server.response.*
 import io.ktor.server.routing.*
 
 fun Route.deleteRecipie(controller: RoomController) {
-    get("/deleteRecipe") {
-        val id = call.parameters["id"] ?: call.respond(HttpStatusCode.NoContent, "id parameter not available")
-        id?.let {  ids ->
-            controller.deleteRecipie(ids.toString())
+    delete("/deleteRecipe") {
+        val id = call.parameters["id"] ?: return@delete
+        id.let {
+            controller.deleteRecipie(it)
         }
+        call.respond(HttpStatusCode.OK, "the data is deleted")
     }
 }
